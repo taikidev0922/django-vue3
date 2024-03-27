0fdfa8fd93ba8409ae283ec9ef21abfa4abb924d
H2W-DEV\nakanishi <t-nakanishi@h2-works.co.jp>
2024-03-27 19:31:36 +0900

    ビルドして画面が表示されるところまで

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,47 +2,52 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_ROOT_URL,
-  timeout: import.meta.env.VITE_TIMEOUT_MS,
+  timeout: import.meta.env.VITE_TIMEOUT_MS || 5000,
   headers: {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest'
   }
 })
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('access')
-    if (token) {
-      config.headers['Authorization'] = `JWT ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
+// 共通前処理
+api.interceptors.request.use((config) => {
+  // 認証用トークンがあればリクエストヘッダに加える
+  const token = localStorage.getItem('access')
+  if (token) {
+    config.headers.Authorization = 'JWT ' + token
   }
-)
+  return config
+})
 
+// 共通後処理
 api.interceptors.response.use(
-  (response) => {
-    return response
-  },
+  (response) => response,
   (error) => {
-    switch (error.response.status) {
-      case 400:
+    // エラーの種類に応じたハンドリングをおこなう
+    switch (error.response?.status) {
+      // バリデーションNG
+      case 400: {
+        // オブジェクトのプロパティの値（メッセージの配列）をフラットな配列に変換
         const messages = Object.values(error.response.data).flat()
         return Promise.reject({ level: 'warning', messages: messages })
-      case 401:
+      }
+      // 認証エラー
+      case 401: {
         const token = localStorage.getItem('access')
         if (token) {
+          // 認証用トークンをlocalStorageから削除
           localStorage.removeItem('access')
-          return Promise.reject(new Error('ログイン有効期限が切れました。再度ログインしてください'))
+          return Promise.reject(new Error('ログイン有効期限切れです。'))
         } else {
-          return Promise.reject(new Error('認証エラー'))
+          return Promise.reject(new Error('認証エラーです。'))
         }
+      }
+      // 権限エラー
       case 403:
-        return Promise.reject(new Error('権限がありません'))
+        return Promise.reject(new Error('権限エラーです。'))
+      // その他のエラー
       default:
-        return Promise.reject(new Error('システムエラー'))
+        return Promise.reject(new Error('想定外のエラーが発生しました。'))
     }
   }
 )
