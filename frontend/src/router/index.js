7640a2d1862d6aaff51b665af31f52fb4505bf1c
H2W-DEV\nakanishi <t-nakanishi@h2-works.co.jp>
2024-03-27 19:17:49 +0900

    buildしたら所定のフォルダに静的ファイルがコピーされるようにした

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useAuthStore } from '@/stores/auth.js'
-import LoginView from '@/views/LoginView.vue'
-import HomeView from '@/views/HomeView.vue'
+import { useAuthStore } from '../stores/auth.js'
+import LoginView from '../views/LoginView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
